faaec348df660f189dd680066a9d34a796e74039
github_username <email_address>
2025-03-09 00:19:03 +0530

    Initial commit

--- svm/app.py
+++ svm/app.py
@@ -3,14 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import pickle
+# import pickle
 import os
 from sklearn.datasets import load_iris
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler, LabelEncoder
+# from sklearn.svm import SVC
+# from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
 import joblib
 
 def load_models():
