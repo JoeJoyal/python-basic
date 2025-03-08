1594d2d5d5e76c0dab50a9426616183417b14162
github_username <email_address>
2025-03-09 01:04:38 +0530

    app.py v3

--- svm/app.py
+++ svm/app.py
@@ -1,17 +1,11 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
-import pickle
-import os
-from sklearn.datasets import load_iris
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+import pandas as pd
 import joblib
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+import os
 
 def load_models():
     model_files = {
